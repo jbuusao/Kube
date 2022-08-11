55d58f81c6674b6a2c2e0018ee7384f072bbffe2
Jean-Paul Buu-Sao <jbuusao@gmail.com>
2022-08-11 16:33:07 +0100

    Added minimum app (Angular)

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@ kubectl create -f database/mongo-d.yml
 kubectl create -f database/mongo-s.yml
 kubectl create -f server/server-d.yml
 kubectl create -f server/server-s.yml
+kubectl create -f app/app-s.yml
--- stop.sh
+++ stop.sh
@@ -2,3 +2,5 @@ kubectl delete -f database/mongo-d.yml
 kubectl delete -f database/mongo-s.yml
 kubectl delete -f server/server-d.yml
 kubectl delete -f server/server-s.yml
+kubectl delete -f app/app-s.yml
+
